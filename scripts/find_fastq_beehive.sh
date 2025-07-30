6630e26b1d784be11c04d4cc98eb8611abf38656
vera-rykalina <verarykalina@gmail.com>
2025-07-30 08:45:57 +0200

    Rename RANGE to FOLDER, update for 20004

--- scripts/find_fastq_beehive.sh
+++ scripts/find_fastq_beehive.sh
@@ -3,19 +3,19 @@
 # Created: 03.07.2025
 # Author: Vera Rykalina
 # Purpurse: Get raw fastq sutible for HIVtime files by IDs from BEEHIVE data. 
-# Usage: ./find_fastq_beehive.sh /mnt/wissdaten/rykalinav/FG18_HIV_NGS_Rohdaten/02_Serokonverter/BEEHIVE/reads_combined_20005
-# where to copy -> /mnt/projekte/rykalinav/FG18_HIV_Pipelines/HIV-phyloTSI/TSI_beehive_raw_20005
+# Usage: ./find_fastq_beehive.sh /mnt/wissdaten/rykalinav/FG18_HIV_NGS_Rohdaten/02_Serokonverter/BEEHIVE/reads_combined_20004
+# where to copy -> /mnt/projekte/rykalinav/FG18_HIV_Pipelines/HIV-phyloTSI/TSI_beehive_raw_20004
 # Comment: Do not use more than 10 files per run (no space on the disk)
 
 
-RANGE="19960"
+FOLDER="20004"
 # Get input data (tsi IDs)
-VAR1=$(cat /scratch/rykalinav/rki_tsi_retraining/data/beehive_for_tsi_${RANGE}.txt)
+VAR1=$(cat /scratch/rykalinav/rki_tsi_retraining/data/beehive_for_tsi_${FOLDER}.txt)
 
-if [ -d "/mnt/projekte/rykalinav/FG18_HIV_Pipelines/HIV-phyloTSI/TSI_beehive_raw_${RANGE}" ]; then rm -Rf /mnt/projekte/rykalinav/FG18_HIV_Pipelines/HIV-phyloTSI/TSI_beehive_raw_${RANGE}; fi
+if [ -d "/mnt/projekte/rykalinav/FG18_HIV_Pipelines/HIV-phyloTSI/TSI_beehive_raw_${FOLDER}" ]; then rm -Rf /mnt/projekte/rykalinav/FG18_HIV_Pipelines/HIV-phyloTSI/TSI_beehive_raw_${FOLDER}; fi
 
 # Create dir for output files
-mkdir -p /mnt/projekte/rykalinav/FG18_HIV_Pipelines/HIV-phyloTSI/TSI_beehive_raw_${RANGE}
+mkdir -p /mnt/projekte/rykalinav/FG18_HIV_Pipelines/HIV-phyloTSI/TSI_beehive_raw_${FOLDER}
 
 
 # This is a loop for copying the target files
@@ -24,7 +24,7 @@ COUNTER=0
 for i in ${VAR1}
     do
     echo "Looking for fastq files with ID ${i}."
-    find "$1" -type f -name "*${i}_[0-2].fastq.gz" -exec cp "{}" "/mnt/projekte/rykalinav/FG18_HIV_Pipelines/HIV-phyloTSI/TSI_beehive_raw_${RANGE}" \;
+    find "$1" -type f -name "*${i}_[1-2].fastq.gz" -exec cp "{}" "/mnt/projekte/rykalinav/FG18_HIV_Pipelines/HIV-phyloTSI/TSI_beehive_raw_${FOLDER}" \;
     let COUNTER++
     done
 
@@ -32,6 +32,6 @@ echo "-------------------------------------------------------------------"
 echo "${COUNTER} IDs are found."
 echo "-------------------------------------------------------------------"
 
-FILE_NUM=$(find /mnt/projekte/rykalinav/FG18_HIV_Pipelines/HIV-phyloTSI/TSI_beehive_raw_${RANGE} -type f | wc -l)
+FILE_NUM=$(find /mnt/projekte/rykalinav/FG18_HIV_Pipelines/HIV-phyloTSI/TSI_beehive_raw_${FOLDER} -type f | wc -l)
 echo "${FILE_NUM} files have been copied."
 echo "-------------------------------------------------------------------"
